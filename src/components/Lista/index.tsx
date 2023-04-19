5321f864a778b8f36ac0a802d8795f182597ec04
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-04-19 05:35:50 -0300

    utilizando component function e usando metodo map para renderizar arrays

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
 
 function Lista() {
-    return (
-        <aside>
-            <h2> Estudos dos dias</h2>
-            <ul>
-                <li>
-                    <h3>
-                    React
-                    </h3>
-                    <span>
-                       02:00:00
-                    </span>
-                </li>
 
-                <li>
-                    <h3>
-                      Javascript
-                    </h3>
-                    <span>
-                       01:00:00
-                    </span>
-                </li>
-            </ul>
-        </aside>
+    const tarefa = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }, {
+        tarefa: 'Javascript',
+        tempo: '01:00:00'
+    }]
+
+
+    return (
+        <div>
+            {tarefa.map((item, index) => (
+                <div>
+                    <h3> {item.tarefa} </h3>
+                    <p> {item.tempo}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
